defa24feb97aff72868e536f9d36f93aa580bc49
Sigve Andre Evensen Skaugvoll <saskaugv@stud.ntnu.no>
2016-02-24 15:21:19 +0100

    added functions to open connection, close connection and push to db

--- dbConnectionTest.py
+++ dbConnectionTest.py
@@ -8,11 +8,23 @@ config = {'user': 'username',
           'database':'coordinates',
           }
 
+#Initialize cnx as empty global variable
+cnx = None
+
+def getConnection():
+    cnx = mysql.connector.connect(**config)
+
+def closeConnetion():
+    cnx.close()
+
 
 def connectToDB():
     try:
-        cnx = mysql.connector.connect(**config)
+        #Try to connect to database with config
+        getConnection()
+        #if connection successful
         print("Connection established to database")
+    #if connection attempt failed, do:
     except mysql.connector.Error as err:
         if(err.errno == errorcode.ER_ACCESS_DENIED_ERROR):
             print("Something is wrong with your user name or password")
@@ -22,8 +34,20 @@ def connectToDB():
             print(err)
 
 
+#Creates testData to be uploaded
+add_testCoordinates = ("INSERT INTO coordinates "
+                       "latitude, longitude "
+                       "VALUES (63.424156, 10.393827)")
 
+def pushToDB():
+    cursor = cnx.cursor()
+    cursor.execute(add_testCoordinates)
+    cnx.commit()
+    cursor.close()
 
 def main():
     connectToDB()
-
+    print("Will now try and push to database")
+    pushToDB()
+    print("Did it work?, if so now I will close the connection")
+    closeConnetion()
