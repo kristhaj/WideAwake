8272fd137d97db15aad04dafeacc291468a9da37
Sigve Andre Evensen Skaugvoll <saskaugv@stud.ntnu.no>
2016-02-24 15:00:46 +0100

    laget funksjon for  og koble til database

--- dbConnectionTest.py
+++ dbConnectionTest.py
@@ -1,1 +1,29 @@
-print("hello")
+import mysql.connector
+from mysql.connector import errorcode
+
+#Connection configurations
+config = {'user': 'username',
+          'password':'password',
+          'host':'ip-adr',
+          'database':'coordinates',
+          }
+
+
+def connectToDB():
+    try:
+        cnx = mysql.connector.connect(**config)
+        print("Connection established to database")
+    except mysql.connector.Error as err:
+        if(err.errno == errorcode.ER_ACCESS_DENIED_ERROR):
+            print("Something is wrong with your user name or password")
+        elif(err.errno == errorcode.ER_BAD_DB_ERROR):
+            print("Database does not exist")
+        else:
+            print(err)
+
+
+
+
+def main():
+    connectToDB()
+
