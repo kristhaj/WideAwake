5f5590f75c502aa43114df2cd276899834c977f7
kristhaj <kristhaj@stud.ntnu.no>
2016-03-01 15:54:50 +0100

    added LEDcontrols

---
+++ Interface/LEDcontrols.py
@@ -0,0 +1,34 @@
+#requires the GPIO library to be setup on the Pi
+
+import time
+import RPi.GPIO as GPIO #library for controlling the Pis I/O pins
+
+greenLED = 1 #pin number of green LED
+redLED = 2 #pin number of red LED
+
+GPIO.setmode(GPIO.BOARD)#enables board pin numbering
+GPIO.setmode(greenLED, GPIO.OUT)#sets which pin is to be used as output
+
+def greenLEDon():
+    GPIO.output(greenLED, True)#Turns on green LED, signaling OK road
+
+def redLEDon():
+    GPIO.output(redLED, True)#Turns on red LED, signaling slippery road
+
+def main(startingUp, delay, slipperyStatus): #startingUp is a boolean that changes when the Pi is ready, slipperyStatus is a boolean telling which LED to light after start up
+    while startingUp: #amount of blinks
+        GPIO.output(greenLED, True)#lights LED
+        time.sleep(delay)#pause
+        GPIO.output(greenLED, False)#turns LED off
+        time.sleep(delay)#pause
+    if piStatus: #checks status of road
+        greenLEDon()
+    else:
+        redLEDon()
+
+    GPIO.cleanup()
+
+
+
+
+
