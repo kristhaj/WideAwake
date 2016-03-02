3a74cd80a59ae101d532628bdf3c0c5078db02db
Sigve Andre Evensen Skaugvoll <saskaugv@stud.ntnu.no>
2016-03-02 16:45:08 +0100

    startet editing the testScript

--- testing_dbConnectionTest.py
+++ testing_dbConnectionTest.py
@@ -6,8 +6,16 @@ import dbConnectionTest
 class MyTestCase(unittest.TestCase):
 
     def test_connection(self):
-        self.assertTrue(dbConnectionTest.connectToDB())
+        self.assertTrue(dbConnectionTest.connectToDB()) #true if connection is established
 
+    def test_pushNoDuplicate(self):
+        self.assertTrue(dbConnectionTest.pushToDB(str(10.123466),str(10.123456))) #make sure these are uniqe (not in db).
+
+    def test_pushDuplicate(self):
+        self.assertFalse(dbConnectionTest.pushToDB(str(63.424156),str(10.393827))) #testing for duplicate coordinates to elgseterbrua. Should return false, because they are in the
+
+    def test_PullFromDB(self):
+        self.assertTrue(dbConnectionTest.pullFromDB()) #true if managed to retrive from database
 
 
 if __name__ == '__main__':
