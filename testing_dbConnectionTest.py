9e69d4639cc6caa3f4e663e46e09a6603cdecc46
Sigve Andre Evensen Skaugvoll <saskaugv@stud.ntnu.no>
2016-03-04 10:56:42 +0100

    Fixed bugs. Creats test objects for each test. Does testing on these objects of dbConnection Script

--- testing_dbConnectionTest.py
+++ testing_dbConnectionTest.py
@@ -6,16 +6,23 @@ import dbConnectionTest
 class MyTestCase(unittest.TestCase):
 
     def test_connection(self):
-        self.assertTrue(dbConnectionTest.connectToDB()) #true if connection is established
+        con1 = dbConnectionTest
+        self.assertTrue(con1.connectToDB()) #true if connection is established
 
     def test_pushNoDuplicate(self):
-        self.assertTrue(dbConnectionTest.pushToDB(str(10.123466),str(10.123456))) #make sure these are uniqe (not in db).
+        con2 = dbConnectionTest
+        con2.connectToDB()
+        self.assertTrue(con2.pushToDB(str(86.123466),str(17.123456))) #make sure these are uniqe (not in db).
 
     def test_pushDuplicate(self):
-        self.assertFalse(dbConnectionTest.pushToDB(str(63.424156),str(10.393827))) #testing for duplicate coordinates to elgseterbrua. Should return false, because they are in the
+        con3 = dbConnectionTest
+        con3.connectToDB()
+        self.assertFalse(con3.pushToDB(str(63.424156),str(10.393827))) #testing for duplicate coordinates to elgseterbrua. Should return false, because they are in the
 
     def test_PullFromDB(self):
-        self.assertFalse(dbConnectionTest.pullFromDB())  #I do not understand why this dosent retrun content from db.
+        con4 = dbConnectionTest
+        con4.connectToDB()
+        self.assertTrue(con4.pullFromDB())
 
 
 
