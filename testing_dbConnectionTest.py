e14d3538103cc227f37b7315345084e6395564cd
ChrJohansen <christian766@gmail.com>
2016-03-09 13:41:03 +0100

    endrer con*.closeConnetion til con*.closeConnection

--- testing_dbConnectionTest.py
+++ testing_dbConnectionTest.py
@@ -8,25 +8,25 @@ class MyTestCase(unittest.TestCase):
     def test_connection(self):
         con1 = dbConnectionTest
         self.assertTrue(con1.connectToDB()) #true if connection is established
-        con1.closeConnetion()
+        con1.closeConnection()
 
     def test_pushNoDuplicate(self):
         con2 = dbConnectionTest
         con2.connectToDB()
-        self.assertTrue(con2.pushToDB(str(86.123466),str(17.123456))) #make sure these are uniqe (not in db).
-        con2.closeConnetion()
+        self.assertTrue(con2.pushToDB(str(33.123466),str(33.123456))) #make sure these are uniqe (not in db).
+        con2.closeConnection()
 
     def test_pushDuplicate(self):
         con3 = dbConnectionTest
         con3.connectToDB()
         self.assertFalse(con3.pushToDB(str(63.424156),str(10.393827))) #testing for duplicate coordinates to elgseterbrua. Should return false, because they are in the
-        con3.closeConnetion()
+        con3.closeConnection()
 
     def test_PullFromDB(self):
         con4 = dbConnectionTest
         con4.connectToDB()
         self.assertTrue(con4.pullFromDB())
-        con4.closeConnetion()
+        con4.closeConnection()
 
 
 if __name__ == '__main__':
