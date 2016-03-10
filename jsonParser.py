30acae43b2c0f371f077c102d85ae582a925229b
kattn <mgbjerke@gmail.com>
2016-03-10 17:52:45 +0100

    Class missed some func in prev commit

--- jsonParser.py
+++ jsonParser.py
@@ -4,30 +4,39 @@ from pprint import pprint
 class JsonParser:
 
 	wantedAttributes = dict()
+	pathName = ""
 
 	def JsonParser(self):
 		self.wantedAttributes = {
-		'accelerator_pedal_position': False,
-		'engine_speed': False,
-		'vehicle_speed': True,
-		'torque_at_transmission': False,
-		'fuel_consumed_since_restart': False,
-		'odometer': False,
-		'fuel_level': False,
-		'steering_wheel_angle': False,
-		'latitude': True,
-		'longitude': True,
-		'transmission_gear_position': False,
-		'brake_pedal_status': False,
-		'door_status': False,
-		'button_state': False,
-		'headlamp_status': False,
-		'windshield_wiper_status': False,
-		'ignition_status': False
-	}
+			'accelerator_pedal_position': False,
+			'engine_speed': False,
+			'vehicle_speed': True,
+			'torque_at_transmission': False,
+			'fuel_consumed_since_restart': False,
+			'odometer': False,
+			'fuel_level': False,
+			'steering_wheel_angle': False,
+			'latitude': True,
+			'longitude': True,
+			'transmission_gear_position': False,
+			'brake_pedal_status': False,
+			'door_status': False,
+			'button_state': False,
+			'headlamp_status': False,
+			'windshield_wiper_status': False,
+			'ignition_status': False
+		}
+		self.pathName = "Resources/downtown-crosstown.json"
+
 
 	def getWantedAttributes(self):
 		return self.wantedAttributes
+
+	def getPath(self):
+		return self.pathName
+
+	def setPath(self, newPath):
+		self.pathName = newPath
 
 	def setWantedAttributes(self, attributeDict):
 		wantedAttributes = attributeDict
@@ -47,7 +56,7 @@ class JsonParser:
 			print(attName)
 
 
-	def getResources(self,pathname):
+	def getResources(self,pathname=pathName):
 		jfile = list()
 		with open(pathname) as f:
 			for line in f:
