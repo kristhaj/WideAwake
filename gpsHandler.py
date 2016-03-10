6ecd93f038eb29f860c6cbb2dccfec80af8006a0
Sigve Andre Evensen Skaugvoll <saskaugv@stud.ntnu.no>
2016-03-10 17:55:33 +0100

    made it a class

--- gpsHandler.py
+++ gpsHandler.py
@@ -3,58 +3,58 @@ import dbConnectionTest
 from geopy.distance import vincenty
 print("end")
 
+class GPSHandler:
+    connection = None
+
+    def setConnection(self,dbConnection):
+        try:
+            self.connection = dbConnection
+            return True
+        except:
+            return False
 
 
+    def compareCoordinates(self, carLat,carLong):
+        try:
+            #find out what 'square'- to put in the where clause in SQL query.
+            minLat, maxLat = self.getBetween(carLat)
+            minLong, maxLong = self.getBetween(carLong)
+            query = ("SELECT Latitude, Longitude FROM Coordinates WHERE (carLat BETWEEN minLat AND maxLat) AND (carLong BETWEEN minLong AND maxLong)")
+            resultSet = self.connection.getResultset(query)
+            self.validateCoordinates(carLat,carLong,resultSet)
+        except Exception as e:
+            return None
 
-def setConnection(dbConnection):
-    global connection
-    try:
-        connection = dbConnection
-        return True
-    except:
-        return False
+    def validateCoordinates(self, carLat, carLong, resultSet):
+        carPos = (carLat, carLong)
+        #check if car coordinates is close to the resultSet (slippery Coordinates)
+        for (lat, long) in resultSet:
+            distKM = vincenty(carPos, (lat,long)).km
+            if(distKM < 1):
+                #calculates new distance to slippery path
+                distM = vincenty(carPos, (lat,long)).meters
+                if(distM <= 100):
+                    return 'C',(lat,long) #C for close
+                else:
+                    return 'A',(lat,long) #A for approaching
 
-
-def compareCoordinates(carLat,carLong):
-    try:
-        #find out what 'square'- to put in the where clause in SQL query.
-        minLat, maxLat = getBetween(carLat)
-        minLong, maxLong = getBetween(carLong)
-        query = ("SELECT Latitude, Longitude FROM Coordinates WHERE (carLat BETWEEN minLat AND maxLat) AND (carLong BETWEEN minLong AND maxLong)")
-        resultSet = connection.getResultset(query)
-        validateCoordinates(carLat,carLong,resultSet)
-    except Exception as e:
-        return None
-
-def validateCoordinates(carLat, carLong, resultSet):
-    carPos = (carLat, carLong)
-    #check if car coordinates is close to the resultSet (slippery Coordinates)
-    for (lat, long) in resultSet:
-        distKM = vincenty(carPos, (lat,long)).km
-        if(distKM < 1):
-            #calculates new distance to slippery path
-            distM = vincenty(carPos, (lat,long)).meters
-            if(distM <= 100):
-                return 'C',(lat,long) #C for close
-            else:
-                return 'A',(lat,long) #A for approaching
-
-    #If not stopped by now, by one of the returns. That means that nearby coordinates are not slippery
-    return 'N',None #N for none
+        #If not stopped by now, by one of the returns. That means that nearby coordinates are not slippery
+        return 'N',None #N for none
 
 
-def getBetween(coordinate):
-    precision = len(str(coordinate).split(".")[1])
-    number = "0."+'0'*(precision-2)+"2"
-    maxNumber = round((coordinate + float(number)),precision)
-    minNumber = round((coordinate - float(number),precision))
-    return minNumber,maxNumber
+    def getBetween(self, coordinate):
+        precision = len(str(coordinate).split(".")[1])
+        number = "0."+'0'*(precision-2)+"2"
+        maxNumber = round((coordinate + float(number)),precision)
+        minNumber = round((coordinate - float(number),precision))
+        return minNumber,maxNumber
 
 
 def main():
     con = dbConnectionTest
     con.getConnection()
-    setConnection(con)
+    handler = GPSHandler()
+    handler.setConnection(con)
 
 main()
 
