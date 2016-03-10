4374bb3f2b4766ef6a9493466da94e1a0e42be94
kattn <mgbjerke@gmail.com>
2016-03-10 16:30:04 +0100

    Added jsonParser.py, work still in progress, but no errors

--- jsonParser.py
+++ jsonParser.py
@@ -1,2 +1,55 @@
 import json
+from pprint import pprint
 
+wantedAttributes = {
+		'accelerator_pedal_position': False,
+		'engine_speed': False,
+		'vehicle_speed': True,
+		'torque_at_transmission': False,
+		'fuel_consumed_since_restart': False,
+		'odometer': False,
+		'fuel_level': False,
+		'steering_wheel_angle': False,
+		'latitude': True,
+		'longitude': True,
+		'transmission_gear_position': False,
+		'brake_pedal_status': False,
+		'door_status': False,
+		'button_state': False,
+		'headlamp_status': False,
+		'windshield_wiper_status': False,
+		'ignition_status': False
+	}
+
+def getWantedAttributes():
+	return wantedAttributes
+
+def setWantedAttributes(attributeDict):
+	wantedAttributes = attributeDict
+
+def addWantedAttribute(attribute,bool):
+	wantedAttributes[attribute] = bool
+
+def removeWantedAttribute(attribute):
+	del wantedAttributes[attribute]
+
+
+def isAttributeWanted(attributeLine):
+	attName = attributeLine.split("\"")[3]
+	if attName in wantedAttributes:
+		return wantedAttributes[attName]
+	else:
+		print(attName)
+
+
+def getResources(pathname):
+	jfile = list()
+	with open(pathname) as f:
+		i = -1
+		for line in f:
+			if isAttributeWanted(line):
+				continue
+			jfile += json.loads(line)
+
+
+getResources("Resources/downtown-crosstown.json")
