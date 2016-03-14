cf00bdea37a272eca73b234146c5afacfe7a247c
Sigve Andre Evensen Skaugvoll <saskaugv@stud.ntnu.no>
2016-03-14 14:42:02 +0100

    Still working on dataset and testing

--- testing_gpsHandler.py
+++ testing_gpsHandler.py
@@ -1,24 +1,48 @@
 import unittest
-import dbConnection
+from dbConnection import DBConnection
 from gpsHandler import GPSHandler
 from jsonParser import JsonParser
+import time
+
 
 class MyTestCase(unittest.TestCase):
+    parser = JsonParser()
+    #list containing dictionaries
+    list = parser.getResources("Resources/downtown-crosstown.json")
 
     def test_parameter(self):
-        con = dbConnection()
+        con = DBConnection()
         con.connectToDB()
         handler = GPSHandler()
         state = handler.setConnection(con)
         self.assertTrue(state)
 
     def test_carPos_against_dbCoordinates(self):
-        con = dbConnection()
+        con = DBConnection()
         con.connectToDB()
         handler = GPSHandler()
-        parser = JsonParser()
-        dictionary = parser.getWantedAttributes()
-        print(dictionary)
+
+
+
+        for i in range(len(list)):
+            time1 = self.list[i]["timestamp"]
+            if(self.list[i+1]["end_of_script"] == True):
+                None
+            else:
+                name = self.list[i]["name"]
+                if(name) == "latitude":
+                    carLat = self.list[i]["latitude"]
+                elif(name) == "longitude":
+                    carLong = self.list[i]["longitude"]
+                handler.compareCoordinates(carLat,carLong)
+
+                #time to sleep between next command --> virtual real time.
+                time2 = [self.list[i+1]["timestamp"]]
+                time.sleep(time2-time1)
+
+
+
+
 
 
 if __name__ == '__main__':
