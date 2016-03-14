ec46b9d08f3d3d03fc7333cdda2393fcbff1aba0
Sigve Andre Evensen Skaugvoll <saskaugv@stud.ntnu.no>
2016-03-14 11:05:06 +0100

    started thinking about test against testData and Jsonparser

--- testing_gpsHandler.py
+++ testing_gpsHandler.py
@@ -1,16 +1,24 @@
 import unittest
 import dbConnection
 from gpsHandler import GPSHandler
+from jsonParser import JsonParser
 
 class MyTestCase(unittest.TestCase):
 
     def test_parameter(self):
-        con = dbConnection
+        con = dbConnection()
         con.connectToDB()
         handler = GPSHandler()
         state = handler.setConnection(con)
         self.assertTrue(state)
 
+    def test_carPos_against_dbCoordinates(self):
+        con = dbConnection()
+        con.connectToDB()
+        handler = GPSHandler()
+        parser = JsonParser()
+        dictionary = parser.getWantedAttributes()
+        print(dictionary)
 
 
 if __name__ == '__main__':
