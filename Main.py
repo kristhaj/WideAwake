6b7ee9fc78a08d8d1c15fbcfc0360965e3a7fcff
ChrJohansen <christian766@gmail.com>
2016-03-16 15:51:00 +0100

    Fortsatt i utforskningsstadiet

--- Main.py
+++ Main.py
@@ -1,10 +1,37 @@
-import Interface.LEDcontrols
-import dbConnectionTest
+#from Interface import LEDcontrols
+from dbConnection import DBConnection
+from gpsHandler import GPSHandler
+from car import Car
+from jsonParser import JsonParser
+import time
+import sys
+
+
+
 
 def main():
-    dbConnection = dbConnectionTest
-    dbConnection.connectToDB()
-    dbConnection.pullFromDB()
-    dbConnection.closeConnection()
+    try:
+        connection = DBConnection()
+        connection.connectToDB()
+        cache = connection.getResultSet("SELECT Latitude,Longitude FROM Coordinates")
+        handler = GPSHandler()
+
+        #car = Car()
+        #car.setTrip(parser.getPath(),parser.getWantedAttributes())
+        #while(car.next()):
+         #   print(car.long)
+
+
+
+    except:
+        print("Kunne ikke koble til database")
+
+   # while(connection):
+    #    print("hei")
+
+    print(cache)
+    connection.closeConnection()
+    parser = JsonParser()
+    car = Car(parser.getPath(),parser.getWantedAttributes())
 
 main()
