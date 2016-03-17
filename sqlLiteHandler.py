6f23ae030e91cb781ffd379ec447fc023b389fba
Sigve Andre Evensen Skaugvoll <saskaugv@stud.ntnu.no>
2016-03-17 10:08:33 +0100

    added documentation, commented out call to main function

--- sqlLiteHandler.py
+++ sqlLiteHandler.py
@@ -7,10 +7,11 @@ import sqlite3
 
 class SQLLite:
     '''
-
+    This class handles the WideAwake local database. Sets, executes, and closes connection with the SQLlite db.
     '''
     def __init__(self,path):
         '''
+        Sets the class static variables
         '''
         self.path = path
         self.connection = None
@@ -18,32 +19,46 @@ class SQLLite:
 
     def establishConnection(self):
         '''
+        Establishes the connection with the SQLLite database, and creates a cursor object to execute statements.
+        :return: True if successful, False else.
         '''
         try:
             self.connection = sqlite3.connect(self.path)
             self.cursor = self.connection.cursor()
+            return True
         except:
-            print('E')
+            return False
 
     def executeInsertStatement(self,query):
         '''
+        Executes the insert statment. Inserts new coordinates into database.
+        :param query: the insert statement to be executed
+        :return: True if execute successful, False if not.
         '''
-        self.cursor.execute(query)
-        self.connection.commit()
+        try:
+            self.cursor.execute(query)
+            self.connection.commit()
+            return True
+        except:
+            return False
 
     def executeQueryStatement(self):
         '''
+        Executes the query statement and returns resultset.
+        :return: resultSet as List if successfull, else False.
         '''
         query = "SELECT * FROM WideAwakeTrip"
-        self.cursor.execute(query)
-        return self.createResultSet()
+        try:
+            self.cursor.execute(query)
+            return self.createResultSet()
+        except:
+            return False
 
     def createResultSet(self):
         '''
-         This function creates the result set. Iterates through the cursor (tuple for tuple)
-         adds the tuples to a list. returns the list
-         :param:
-         :return: resultSet, list containg tuples from query.
+        This function creates the result set. Iterates through the cursor (tuple for tuple)
+        adds the tuples to a list. returns the list
+        :return: ResultSet as a list containg tuples from database.
         '''
         resultSet = []
         for row in self.cursor:
@@ -52,11 +67,23 @@ class SQLLite:
 
     def closeConnection(self):
         '''
+        Commits latest changes, and closes connection.
+        :return: True if successfully closed commited and closed connection. False else
         '''
-        self.connection.commit()
-        self.connection.close()
+        try:
+            self.connection.commit()
+            self.connection.close()
+            return True
+        except:
+            return False
 
     def createInsertStatment(self,lat,long):
+        '''
+        Creates a insert statment with the given coordinates.
+        :param lat: Car latitude to push to local database.
+        :param long: Car longitude to push to local database
+        :return: SQLlite insert-statment.
+        '''
         return "insert into WideAwakeTrip values ("+str(lat)+","+str(long)+")"
 
 def main():
@@ -71,4 +98,4 @@ def main():
     print(rs)
     conn.closeConnection()
 
-main()
+#main()
