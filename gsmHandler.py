dc430f2825629c05ac07015eeabbbb56dee92938
Bjerke <mgbjerke@gmail.com>
2016-04-01 11:20:04 +0200

    Started working on _getPort()

--- gsmHandler.py
+++ gsmHandler.py
@@ -11,17 +11,35 @@ from gsmmodem.exceptions import TimeoutException, PinRequiredError, IncorrectPin
 
 class GSMHandler(object):
     def __init__(self):
-      self.port = self.getPort()
-      self.baud = 115200
-      self.pin = 1235
-      self.deliver = False
+        self.port = self.getPort()
+        self.baud = 115200
+        self.pin = 1235
+        self.deliver = False
+
+        if self.port == None:
+            raise PinRequiredError("getPort did not find a used port")
+
 
     def _getPort(self):
         '''
 
         :return: port
         '''
-        return None
+        port = None
+
+
+        """
+        Search for ports using a regular expression. Port name, description and
+        hardware ID are searched. The function returns an iterable that returns the
+        same tuples as comport() would do.
+
+        r = re.compile(regexp, re.I)
+        for info in comports():
+            port, desc, hwid = info
+            if r.search(port) or r.search(desc) or r.search(hwid):
+                yield info
+        """
+        return port
 
 
     def sendThatShit(self):
